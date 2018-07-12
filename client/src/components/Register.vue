37892c3e8d7b0cfcbd7cb18c6c2c0f5a3f1683c6
Malik Abdul Aziz <malikabdulaziz51@gmail.com>
2018-07-12 20:30:41 +0700

    Setting up database and registration function

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="">
-    <h1>Register</h1>
+  <v-layout row >
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-tittle>Register</v-toolbar-tittle>
+        </v-toolbar>
 
-    <input type="email"
-    v-model="email"
-    name="email"
-    placeholder="email"><br>
-
-    <input type="password"
-    v-model="password"
-    name="password"
-    placeholder="password"><br>
-
-    <button type="button" name="submit" @click="register">Register</button>
-  </div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email"
+          v-model="email"
+          name="email"
+          placeholder="email">
+          <br>
+          <input type="password"
+          v-model="password"
+          name="password"
+          placeholder="password">
+          <br>
+          <div class="error" v-html="error">
+          </div>
+          <br>
+          <button type="button"
+          name="submit"
+          @click="register">
+          Register
+          </button>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -22,31 +37,28 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
-    }
-  },
-  watch: {
-    email(value){
-      console.log('email has change', value)
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.email = ''
-    }, 2000)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
